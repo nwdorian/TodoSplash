c1d9be373f96f4798636248f1aac2ba79379c995
nwdorian <neuwirthdorian@gmail.com>
2026-01-22 00:56:04 +0100

    feat: add GetById feature

---
+++ TodoSplash.Api/Todos/GetById.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoSplash.Api.Data;
+using TodoSplash.Api.Endpoints;
+
+namespace TodoSplash.Api.Todos;
+
+public static class GetById
+{
+    public record Response(int Id, string Name, bool IsComplete);
+
+    public class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("todos", Handler).WithTags("Todos");
+        }
+
+        public static async Task<IResult> Handler(int id, TodoContext context, CancellationToken cancellationToken)
+        {
+            Todo? todo = await context.Todos.AsNoTracking().FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+
+            if (todo is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            return TypedResults.Ok(new Response(todo.Id, todo.Name, todo.IsComplete));
+        }
+    }
+}
