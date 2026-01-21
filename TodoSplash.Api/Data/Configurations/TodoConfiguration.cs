785ccd594ee34945bbf0d2fd7b8a56c037759fd2
nwdorian <neuwirthdorian@gmail.com>
2026-01-21 19:21:55 +0100

    chore: add InitialCreate migration

---
+++ TodoSplash.Api/Data/Configurations/TodoConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoSplash.Api.Models;
+
+namespace TodoSplash.Api.Data.Configurations;
+
+public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+{
+    public void Configure(EntityTypeBuilder<Todo> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+        builder.Property(t => t.IsComplete).HasDefaultValue(false);
+    }
+}
