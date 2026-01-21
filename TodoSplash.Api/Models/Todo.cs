52009920a80d1d4f3c009cc3606fc11bdadca593
nwdorian <neuwirthdorian@gmail.com>
2026-01-21 17:40:53 +0100

    feat: add Todo model

---
+++ TodoSplash.Api/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoSplash.Api.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public bool IsComplete { get; set; }
+}
