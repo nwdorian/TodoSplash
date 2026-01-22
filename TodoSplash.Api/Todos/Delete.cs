a612abde6d4d9db8a82ad4bcea93415286560906
nwdorian <neuwirthdorian@gmail.com>
2026-01-22 01:07:05 +0100

    feat: add Delete feature

---
+++ TodoSplash.Api/Todos/Delete.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoSplash.Api.Data;
+using TodoSplash.Api.Endpoints;
+
+namespace TodoSplash.Api.Todos;
+
+public static class Delete
+{
+    public class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("todos/{id}", Handler).WithTags("Todos");
+        }
+
+        public static async Task<IResult> Handler(int id, TodoContext context, CancellationToken cancellationToken)
+        {
+            Todo? todo = await context.Todos.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+            if (todo is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            context.Todos.Remove(todo);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return TypedResults.NoContent();
+        }
+    }
+}
